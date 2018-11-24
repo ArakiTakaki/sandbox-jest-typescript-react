ae55e515318ef16004632185fea227af0267d854
ArakiTakaki <araki_takaki@yahoo.co.jp>
2018-11-24 19:55:39 +0900

    webpackのproductioを設定

---
+++ webpack.production.js
@@ -0,0 +1,24 @@
+const resolves = require("./config/resolves");
+const loaders = require("./config/loaders");
+const plugins = require("./config/plugins");
+const path = require("path");
+
+const WebpackObfuscator = require("webpack-obfuscator");
+
+module.exports = {
+  mode: "production",
+  entry: ["./src/ts/index.tsx",],
+  module: {
+    rules: loaders
+  },
+  resolve: resolves,
+  plugins: [
+    ...plugins,
+    new WebpackObfuscator({ rotateUnicodeArray: true })
+  ],
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  }
+};
